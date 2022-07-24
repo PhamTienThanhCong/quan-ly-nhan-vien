c1b40354a15eb3e042a4f2e275c8971ca21c90a6
congphamtienthanh <congphamtienthanh@gmail.com>
2022-07-24 12:00:29 +0700

    api user

--- src/controllers/uc002Controller.php
+++ src/controllers/uc002Controller.php
@@ -1,6 +1,12 @@
 <?php
 class uc002Controller extends Controllers{
+    public function middleware(){
+        if (!isset($_SESSION['id'])){
+            header("Location: " . $this->host_name);
+        }
+    }
     public function index(){
+        $this->middleware();
         $this->view("main","UC002/index",[]);
     }
 }
