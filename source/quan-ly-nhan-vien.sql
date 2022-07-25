cbe10974669e24916f164693e01cb74f644de7a1
congphamtienthanh <congphamtienthanh@gmail.com>
2022-07-25 21:32:27 +0700

    update database

--- source/quan-ly-nhan-vien.sql
+++ source/quan-ly-nhan-vien.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost:3306
--- Thời gian đã tạo: Th7 21, 2022 lúc 03:50 AM
+-- Thời gian đã tạo: Th7 25, 2022 lúc 02:31 PM
 -- Phiên bản máy phục vụ: 5.7.33
 -- Phiên bản PHP: 7.4.19
 
@@ -103,20 +103,20 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`EMPLOYEE_ID`, `DEPART_ID`, `MANAGER_ID`, `NAME`, `EMAIL`, `PHONE`, `SALARY_RATE`, `TYPE`, `USERNAME`, `PASSWORD`, `TITLE`, `STATUS`, `ADDRESS`, `CMND/CCCD`, `START_DATE`, `ROLE`, `AVATAR_URL`, `END_DATE`, `DOB`) VALUES
-(1, 1, 5, 'Mã Vân Tiên', 'mavantien801@gmail.com', '0798234759', '30000000', 'Full-time', 'mavantien', '4QtPn5cVDr5JK9bUuZ', 'Accounting Manager', 'Enabled', '21 Xã Thanh Hà, Huyện Thanh Liêm, Hà Nam', '0123456789', '2018-07-01 09:00:00', 'Manager', 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80', NULL, '1990-07-04'),
-(2, 2, 5, 'Nguyễn Thị Thùy Dương', 'nguyenthithuyduong@gmail.com', '0919007159', '27000000', 'Full-time', 'nguyenthithuyduong', '12QtPn5cVDr5JK9bUuZ', 'Head Of International Relations', 'Enabled', '65 Trung Nhi Street, Le Loi Ward', '0299958918', '2017-12-01 09:00:00', 'Manager', 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1981-12-12'),
-(3, 3, 5, 'Ân Hải Nam', 'anhainam727@gmail.com', '0901863279', '35000000', 'Full-time', 'anhainam', '8QG$@H$9&zgb4s', 'Senior HRBP ', 'Enabled', 'Xã Bằng Cốc, Huyện Hàm Yên, Tuyên Quang', '013890451', '2016-07-02 13:42:33', 'Manager', 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80', NULL, '1995-11-25'),
-(4, 4, 5, 'Đỗ Khánh Vi', 'dokhanhvi850@gmail.com', '0597623841', '50000000', 'Full-time', 'dokhanhvi', '8^UUGaK68m', 'IT Lead', 'Enabled', '29A Phường Cam Lộc, Thành phố Cam Ranh, Khánh Hòa', '0987654321', '2017-05-31 08:30:00', 'Manager', 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=461&q=80', NULL, '1981-10-04'),
-(5, 5, NULL, 'Phan Thị Nguyên Trang', 'sophiepham2k@gmail.com', '0943807159', '60000000', 'Full-time', 'admin', '123456789', 'Company Chief Executive Officer', 'Enabled', 'Xã Bình Thủy, Huyện Châu Phú, An Giang', '0312458907', '2012-05-01 12:00:00', 'CEO', 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1975-01-01'),
-(6, 4, 4, 'Trần Chí Dũng', 'tranchidung@gmail.com', '0959107159', '15000000', 'Full-time', 'tranchidung', 'pANICEALvAnE', 'Project Manager', 'Enabled', '12B Xã Bình Thủy, Huyện Châu Phú, An Giang', '0387958907', '2019-05-01 12:00:00', 'Manager', 'https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1989-01-05'),
-(7, 4, 6, 'Hoàng Trúc Linh', 'hoangtruclinh@gmail.com', '0989007159', '10000000', 'Part-time', 'hoangtruclinh', 'sEnEWorBIaLZeR', 'Data Analyst', 'Enabled', '65 Trung Nhi Street, Le Loi Ward', '0387958918', '2022-06-01 09:00:00', 'Employee', 'https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80', NULL, '1999-12-05'),
-(8, 4, 6, 'Nguyễn Trung Tín', 'nguyentrungtin@gmail.com', '0919127159', '13500000', 'Seasonal', 'nguyentrungtin', '@E&jG$SaRxMWvd', 'Software Engineer', 'Enabled', '89A Hoang Van Thu Street, CMT8 Ward', '0212458917', '2021-12-30 09:00:00', 'Employee', 'https://images.unsplash.com/photo-1556157382-97eda2d62296?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1999-12-12'),
-(9, 4, 6, 'Trần Minh Tiến', 'tranminhtien@gmail.com', '0901890456', '12000000', 'Full-time', 'tranminhtien', 'sOcabEnTEnUcke', 'Software Engineer', 'Enabled', '5 Nguyen Binh Khiem Street, Cam Linh Ward', '0123490192', '2022-06-01 17:37:41', 'Employee', 'https://images.unsplash.com/photo-1543132220-3ec99c6094dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1997-11-04'),
-(10, 3, 3, 'Nguyễn Thị Linh Chi', 'nguyenthilinhchi@gmail.com', '0908789032', '13000000', 'Temporary', 'nguyenthilinhchi', 'FOcabEnTEnUckf', 'HRBP Manager Intern', 'Disabled', '227 Nguyễn Văn Cừ P.5 Q.5 TP.HCM', '0981958907', '2021-12-01 10:41:33', 'Employee', 'https://images.unsplash.com/photo-1514315384763-ba401779410f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=383&q=80', '2022-07-16 10:36:55', '1999-12-30'),
-(12, 4, 6, 'Đỗ Ngọc Nga', 'dongocnga@gmail.com', '0346868990', '6000000', 'Full-time', 'dongocnga', 'sOcabEnTEnUcke', 'Data Scientist Intern', 'Disabled', '210A CMT8 P.15 Q.Tân Bình TP.HCM', '0290858918', '2021-07-01 10:41:40', 'Employee', 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', '2022-07-17 10:41:40', '2001-01-01'),
-(13, 4, 6, 'Nguyễn Thị Minh Tú', 'nguyenthiminhtu@gmail.com', '0586484992', '35000000', 'Temporary', 'nguyenthiminhtu', 'tgDARcfbMbhmHY', 'Software Architecture', 'Enabled', '19A Nguyễn Thị Minh Khai Q.3 TP.HCM', '0931490192', '2020-07-31 10:41:40', 'Employee', 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1984-09-12'),
-(14, 4, 6, 'Satoh Takeru', 'satohtakeru@gmail.com', '0918234744', '40000000', 'Full-time', 'satohtakeru', 'SiDARcfbMbhAAA', 'Senior Software Engineer', 'Enabled', '123 Nguyễn Văn Cừ P.5 Q.5 TP.HCM', '02001490192', '2020-06-01 10:52:17', 'Employee', 'https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1995-10-26'),
-(15, 4, 6, 'Đinh Ngọc Phi Linh', 'dinhngocphilinh@gmail.com', '0957807123', '20500000', 'Seasonal', 'dinhngocphilinh', 'u5ZDu56GQMF79285TF', 'Junior Data Scientist', 'Enabled', '289 Cộng Hòa Q.Tân Bình TP.HCM', '02921490192', '2021-07-01 10:56:05', 'Employee', 'https://images.unsplash.com/photo-1563306406-e66174fa3787?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1993-05-04');
+(1, 1, 5, 'Mã Vân Tiên', 'mavantien801@gmail.com', '0798234759', '30000000', 'Full-time', 'mavantien', 'cong', 'Accounting Manager', 'Enabled', '21 Xã Thanh Hà, Huyện Thanh Liêm, Hà Nam', '0123456789', '2018-07-01 09:00:00', 'Manager', 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80', NULL, '1990-07-04'),
+(2, 2, 5, 'Nguyễn Thị Thùy Dương', 'nguyenthithuyduong@gmail.com', '0919007159', '27000000', 'Full-time', 'nguyenthithuyduong', 'cong', 'Head Of International Relations', 'Enabled', '65 Trung Nhi Street, Le Loi Ward', '0299958918', '2017-12-01 09:00:00', 'Manager', 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1981-12-12'),
+(3, 3, 5, 'Ân Hải Nam', 'anhainam727@gmail.com', '0901863279', '35000000', 'Full-time', 'anhainam', 'cong', 'Senior HRBP ', 'Enabled', 'Xã Bằng Cốc, Huyện Hàm Yên, Tuyên Quang', '013890451', '2016-07-02 13:42:33', 'Manager', 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80', NULL, '1995-11-25'),
+(4, 4, 5, 'Đỗ Khánh Vi', 'dokhanhvi850@gmail.com', '0597623841', '50000000', 'Full-time', 'dokhanhvi', 'cong', 'IT Lead', 'Enabled', '29A Phường Cam Lộc, Thành phố Cam Ranh, Khánh Hòa', '0987654321', '2017-05-31 08:30:00', 'Manager', 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=461&q=80', NULL, '1981-10-04'),
+(5, 5, NULL, 'Phan Thị Nguyên Trang', 'sophiepham2k@gmail.com', '0943807159', '60000000', 'Full-time', 'admin', 'cong', 'Company Chief Executive Officer', 'Enabled', 'Xã Bình Thủy, Huyện Châu Phú, An Giang', '0312458907', '2012-05-01 12:00:00', 'CEO', 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1975-01-01'),
+(6, 4, 4, 'Trần Chí Dũng', 'tranchidung@gmail.com', '0959107159', '15000000', 'Full-time', 'tranchidung', 'cong', 'Project Manager', 'Enabled', '12B Xã Bình Thủy, Huyện Châu Phú, An Giang', '0387958907', '2019-05-01 12:00:00', 'Manager', 'https://images.unsplash.com/photo-1507591064344-4c6ce005b128?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1989-01-05'),
+(7, 4, 6, 'Hoàng Trúc Linh', 'hoangtruclinh@gmail.com', '0989007159', '10000000', 'Part-time', 'hoangtruclinh', 'cong', 'Data Analyst', 'Enabled', '65 Trung Nhi Street, Le Loi Ward', '0387958918', '2022-06-01 09:00:00', 'Employee', 'https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80', NULL, '1999-12-05'),
+(8, 4, 6, 'Nguyễn Trung Tín', 'nguyentrungtin@gmail.com', '0919127159', '13500000', 'Seasonal', 'nguyentrungtin', 'cong', 'Software Engineer', 'Enabled', '89A Hoang Van Thu Street, CMT8 Ward', '0212458917', '2021-12-30 09:00:00', 'Employee', 'https://images.unsplash.com/photo-1556157382-97eda2d62296?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', NULL, '1999-12-12'),
+(9, 4, 6, 'Trần Minh Tiến', 'tranminhtien@gmail.com', '0901890456', '12000000', 'Full-time', 'tranminhtien', 'cong', 'Software Engineer', 'Enabled', '5 Nguyen Binh Khiem Street, Cam Linh Ward', '0123490192', '2022-06-01 17:37:41', 'Employee', 'https://images.unsplash.com/photo-1543132220-3ec99c6094dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1997-11-04'),
+(10, 3, 3, 'Nguyễn Thị Linh Chi', 'nguyenthilinhchi@gmail.com', '0908789032', '13000000', 'Temporary', 'nguyenthilinhchi', 'cong', 'HRBP Manager Intern', 'Disabled', '227 Nguyễn Văn Cừ P.5 Q.5 TP.HCM', '0981958907', '2021-12-01 10:41:33', 'Employee', 'https://images.unsplash.com/photo-1514315384763-ba401779410f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=383&q=80', '2022-07-16 10:36:55', '1999-12-30'),
+(12, 4, 6, 'Đỗ Ngọc Nga', 'dongocnga@gmail.com', '0346868990', '6000000', 'Full-time', 'dongocnga', 'cong', 'Data Scientist Intern', 'Disabled', '210A CMT8 P.15 Q.Tân Bình TP.HCM', '0290858918', '2021-07-01 10:41:40', 'Employee', 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', '2022-07-17 10:41:40', '2001-01-01'),
+(13, 4, 6, 'Nguyễn Thị Minh Tú', 'nguyenthiminhtu@gmail.com', '0586484992', '35000000', 'Temporary', 'nguyenthiminhtu', 'cong', 'Software Architecture', 'Enabled', '19A Nguyễn Thị Minh Khai Q.3 TP.HCM', '0931490192', '2020-07-31 10:41:40', 'Employee', 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1984-09-12'),
+(14, 4, 6, 'Satoh Takeru', 'satohtakeru@gmail.com', '0918234744', '40000000', 'Full-time', 'satohtakeru', 'cong', 'Senior Software Engineer', 'Enabled', '123 Nguyễn Văn Cừ P.5 Q.5 TP.HCM', '02001490192', '2020-06-01 10:52:17', 'Employee', 'https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1995-10-26'),
+(15, 4, 6, 'Đinh Ngọc Phi Linh', 'dinhngocphilinh@gmail.com', '0957807123', '20500000', 'Seasonal', 'dinhngocphilinh', 'cong', 'Junior Data Scientist', 'Enabled', '289 Cộng Hòa Q.Tân Bình TP.HCM', '02921490192', '2021-07-01 10:56:05', 'Employee', 'https://images.unsplash.com/photo-1563306406-e66174fa3787?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', NULL, '1993-05-04');
 
 -- --------------------------------------------------------
 
@@ -275,7 +275,7 @@ CREATE TABLE `request ot` (
   `EMPLOYEE_ID` int(11) NOT NULL COMMENT 'Chứa ID của nhân viên tạo ra request OT',
   `MANAGER_ID` int(11) NOT NULL COMMENT 'Chứa ID của nhân viên quản lý cấp 1 của nhân viên tạo ra request',
   `REASON` varchar(5000) COLLATE utf8mb4_vietnamese_ci NOT NULL COMMENT 'Lý do nhân viên muốn làm việc OT',
-  `CREATE_DATE` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT 'Thời gian tạo request OT',
+  `CREATE_DATE` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Thời gian tạo request OT',
   `UPDATE_DATE` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT 'Thời gian cập nhật thông tin của request OT gần nhất. Mặc định là 1970-01-01 00:00:00, khi được cập nhật lại, thời gian này phải >= CREATE_DATE',
   `STATUS` varchar(255) COLLATE utf8mb4_vietnamese_ci NOT NULL DEFAULT 'Draft' COMMENT 'Trạng thái xử lý của request OT. Gồm 1 trong 5 trạng thái: Draft, Pending, Approved, Rejected, Cancelled. Được điền tự động bởi hệ thống, nhân viên không thể sửa.',
   `MANAGER_COMMENT` varchar(5000) COLLATE utf8mb4_vietnamese_ci DEFAULT 'None' COMMENT 'Ghi lí do quản lý cấp 1 từ chối request OT. Nếu trường này có thông tin, trạng thái của request phải là "Rejected", mặc định là None',
@@ -291,11 +291,14 @@ CREATE TABLE `request ot` (
 --
 
 INSERT INTO `request ot` (`ROT_ID`, `EMPLOYEE_ID`, `MANAGER_ID`, `REASON`, `CREATE_DATE`, `UPDATE_DATE`, `STATUS`, `MANAGER_COMMENT`, `START_DATE`, `ESTIMATED_HOURS`, `END_DATE`, `UNSUBMIT_REASON`, `NOTIFICATION_FLAG`) VALUES
-(1, 8, 6, 'Dự án mới sắp ra cần hoàn thành gấp để giao vào đầu tháng 8', '2022-07-11 17:40:03', '2022-07-11 17:40:03', 'Draft', 'None', '2022-07-27', '8', '2022-07-31', 'None', 1),
-(2, 15, 6, 'Làm không kịp nên tăng ca để kịp tiến độ dự án mới', '2022-07-17 12:08:03', '2022-07-18 12:08:03', 'Pending', 'None', '2022-07-16', '4', '2022-07-16', 'None', 1),
+(2, 15, 6, 'Làm không kịp nên tăng ca để kịp tiến độ dự án mới', '2022-07-17 12:08:03', '2022-07-18 12:08:03', 'Approved', 'None', '2022-07-16', '4', '2022-07-16', 'None', 1),
 (3, 14, 6, 'Anh có việc gia đình cần nghỉ 1 tuần nên anh cần tăng ca để làm bù để kịp tiến độ dự án.', '2022-07-15 12:12:35', '2022-07-15 13:12:35', 'Approved', 'None', '2022-08-01', '6', '2022-08-03', 'None', 1),
-(4, 8, 6, 'Dự án Alola cần bàn giao cho khách hàng gấp vào cuối tháng 8 nên em cần OT để kịp tiến độ', '2022-07-17 12:14:54', '2022-07-17 12:25:54', 'Draft', 'None', '2022-07-25', '12', '2022-07-27', 'None', 1),
-(5, 8, 6, 'Em xin OT để tuần sau em nghỉ đi chơi với người yêu', '2022-07-16 12:14:54', '2022-07-17 15:25:54', 'Cancelled', 'None', '2022-08-15', '4', '2022-08-18', 'Lí do em ghi còn chưa thật sự hợp lí, em xin được thu hồi request để gửi lại request mới', 0);
+(4, 8, 6, 'Dự án Alola cần bàn giao cho khách hàng gấp vào cuối tháng 8 nên em cần OT để kịp tiến độ', '2022-07-17 12:14:54', '2025-07-22 02:21:13', 'Pending', '', '2022-07-25', '12', '2022-07-25', '', 1),
+(5, 8, 6, 'Em xin OT để tuần sau em nghỉ đi chơi với người yêu', '2022-07-16 12:14:54', '2022-07-17 15:25:54', 'Cancelled', 'None', '2022-08-15', '4', '2022-08-18', 'Lí do em ghi còn chưa thật sự hợp lí, em xin được thu hồi request để gửi lại request mới', 0),
+(12, 15, 6, 'demo thử lần 2', '1970-01-01 00:00:00', '2025-07-22 10:17:49', 'Pending', '', '2022-07-25', '8', '2022-07-27', '', 1),
+(13, 15, 6, 'demo lan 4', '2022-07-25 18:02:19', '2025-07-22 11:02:19', 'Draft', '', '2022-07-25', '0', '2022-07-25', '', 0),
+(14, 15, 6, 'demo ', '2022-07-25 20:48:25', '2025-07-22 01:48:25', 'Pending', '', '2022-07-25', '0', '2022-07-25', '', 0),
+(17, 8, 6, 'lo xoa re quyes hehe', '2022-07-25 20:55:39', '2025-07-22 02:22:42', 'Pending', '', '2022-07-25', '10', '2022-07-25', '', 1);
 
 -- --------------------------------------------------------
 
@@ -315,19 +318,20 @@ CREATE TABLE `request ot detail` (
 --
 
 INSERT INTO `request ot detail` (`ROTDETAIL_ID`, `ROT_ID`, `DATE`, `HOUR`) VALUES
-(1, 1, '2022-07-27', '4'),
-(2, 1, '2022-07-31', '4'),
-(3, 4, '2022-07-25', '4'),
-(4, 4, '2022-07-26', '4'),
-(5, 4, '2022-07-27', '4'),
 (6, 5, '2022-08-15', '1'),
 (7, 5, '2022-08-16', '1'),
-(8, 5, '2022-08-17', '1'),
-(9, 5, '2022-08-18', '1'),
 (10, 2, '2022-07-16', '4'),
-(11, 3, '2022-08-01', '3'),
 (12, 3, '2022-08-02', '2'),
-(13, 3, '2022-08-03', '1');
+(13, 3, '2022-08-03', '1'),
+(34, 12, '2022-07-25', '3'),
+(35, 12, '2022-07-26', '4'),
+(36, 12, '2022-07-26', '2'),
+(37, 13, '2022-07-25', '1'),
+(38, 14, '2022-07-25', '4'),
+(101, 4, '2022-07-25', '1'),
+(102, 4, '2022-07-25', '1'),
+(105, 17, '2022-07-25', '3'),
+(106, 17, '2022-07-26', '4');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -460,13 +464,13 @@ ALTER TABLE `pa_goal detail`
 -- AUTO_INCREMENT cho bảng `request ot`
 --
 ALTER TABLE `request ot`
-  MODIFY `ROT_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Mã của 1 request OT, là trường tự tăng', AUTO_INCREMENT=6;
+  MODIFY `ROT_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Mã của 1 request OT, là trường tự tăng', AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT cho bảng `request ot detail`
 --
 ALTER TABLE `request ot detail`
-  MODIFY `ROTDETAIL_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID của một chi tiết request OT. Một request OT có thể có nhiều thông tin chi tiết', AUTO_INCREMENT=14;
+  MODIFY `ROTDETAIL_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID của một chi tiết request OT. Một request OT có thể có nhiều thông tin chi tiết', AUTO_INCREMENT=110;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
