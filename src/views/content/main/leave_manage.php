11c4c134517d02020aa9ac7fb84ee18c7f94cd33
congphamtienthanh <congphamtienthanh@gmail.com>
2022-07-30 01:34:01 +0700

    up date

--- src/views/content/main/leave_manage.php
+++ src/views/content/main/leave_manage.php
@@ -2,7 +2,7 @@
 <div class="content">
     <h2>leave management</h2>
     <div class="content-cate">
-        <a href="myLeave.html">my leave</a>
+        <a href="<?= $host_name ?>/uc010/index">my leave</a>
         <a href="#">create leave request</a>
     </div>
 </div>
